7bbabec35c8421617493e841d36e40dd5fbcd854
PufAnimalsDev <93289768+PufAnimalsDev@users.noreply.github.com>
2023-06-28 20:47:32 +0200

    add hash router for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import { GlobalContextProvider } from './context/GlobalContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <GlobalContextProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </GlobalContextProvider>,
 );
